0f44832c9a5587abceccf1b24888d19c24b63d0e
Alonso Ramos <alonso24005@hotmail.com>
2024-06-11 22:13:03 -0500

    primer commit

--- BoletaPago.py
+++ BoletaPago.py
@@ -1,19 +1,13 @@
 class Trabajador:
-    def __init__(self,_nombre,_categoria,_horas_extras,_tardanzas):
+    def __init__(self, _nombre, _categoria, _horas_extras, _tardanzas):
         self.nombre = _nombre
-        self.categoria =_categoria
-        self.horas_extras =_horas_extras
-        self.tardanzas =_tardanzas
+        self.categoria = _categoria
+        self.horas_extras = _horas_extras
+        self.tardanzas = _tardanzas
 
     def calcular_sueldo_basico(self):
-        if self.categoria == "A":
-            return 3000
-        elif self.categoria == "B":
-            return 2500
-        elif self.categoria == "C":
-            return 2000
-        else:
-            return 0
+        sueldos = {"A": 3000, "B": 2500, "C": 2000}
+        return sueldos.get(self.categoria, 0)
 
     def calcular_pago_hora_extra(self):
         sueldo_basico = self.calcular_sueldo_basico()
@@ -39,12 +33,12 @@ class Boleta(Trabajador):
         sueldo_neto = self.calcular_sueldo_neto()
 
         print("*** BOLETA DE PAGO ***")
-        print("NOMBRE:", self.nombre)
-        print("CATEGORIA:", self.categoria)
-        print("SUELDO BASICO:", sueldo_basico)
-        print("DESCUENTO TARDANZAS:", '{:,.2f}'.format(descuento_tardanzas))
-        print("PAGO HORAS EXTRAS:", '{:,.2f}'.format(pago_hora_extra))
-        print("SUELDO NETO:", '{:,.2f}'.format(sueldo_neto))
+        print('{:<20}: {}'.format("NOMBRE", self.nombre))
+        print('{:<20}: {}'.format("CATEGORIA", self.categoria))
+        print('{:<20}: {:,.2f}'.format("SUELDO BASICO", sueldo_basico))
+        print('{:<20}: {:,.2f}'.format("DESCUENTO TARDANZAS", descuento_tardanzas))
+        print('{:<20}: {:,.2f}'.format("PAGO HORAS EXTRAS", pago_hora_extra))
+        print('{:<20}: {:,.2f}'.format("SUELDO NETO", sueldo_neto))
 
 nombre = input("Ingrese el nombre del trabajador: ")
 categoria = input("Ingrese la categoría del trabajador (A, B o C): ")
@@ -52,12 +46,10 @@ horas_extras = float(input("Ingrese el número de horas extras trabajadas: "))
 tardanzas = int(input("Ingrese el total de minutos de tardanzas: "))
 
 print("*** DATOS DE ENTRADA ***")
-print("TRABAJADOR:", nombre)
-print("CATEGORIA:", categoria)
-print("HORAS EXTRAS:", horas_extras)
-print("TARDANZAS (minutos):", tardanzas)
+print('{:<20}: {}'.format("TRABAJADOR", nombre))
+print('{:<20}: {}'.format("CATEGORIA", categoria))
+print('{:<20}: {:.2f}'.format("HORAS EXTRAS", horas_extras))
+print('{:<20}: {}'.format("TARDANZAS (minutos)", tardanzas))
 
 boleta_trabajador = Boleta(nombre, categoria, horas_extras, tardanzas)
 boleta_trabajador.generar_boleta_pago()
-
-
